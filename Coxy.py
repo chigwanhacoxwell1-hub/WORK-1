6df0ff9912c6a54ce8fcf4bdd5cfb3a3ab06dffc
chigwanhacoxwell1-hub <chigwanhacoxwell1@gmail.com>
2025-08-22 12:33:50 +0200

    Add files via upload

---
+++ Coxy.py
@@ -0,0 +1,12 @@
+# Constants
+SCALE_FACTOR = 2
+
+# Variables
+length = 10
+width = 5
+
+# Calculate perimeter
+perimeter = SCALE_FACTOR * (length + width)
+
+# Print the result
+print("The perimeter of the rectangle with length", length, "and width", width, "is:", perimeter)
